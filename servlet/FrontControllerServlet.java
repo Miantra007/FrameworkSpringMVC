3174c631416892982715f9d85342f7fccbe0c7b4
RAKOTONIAINA Miantra Vahatrasoa <rakotoniainami5@gmail.com>
2026-06-17 08:25:37 +0300

    fonction init

--- servlet/FrontControllerServlet.java
+++ servlet/FrontControllerServlet.java
@@ -2,23 +2,40 @@ package servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
+import util.Utilitaire;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FrontControllerServlet extends HttpServlet {
+
+    List<String> listController = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        recupererUrl(request, response);
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        recupererUrl(request, response);
+        processRequest(request, response);
     }
 
-    private void recupererUrl(HttpServletRequest request, HttpServletResponse response)
+    public void init() {
+        String packageName = getServletConfig().getInitParameter("controller-package");
+
+        Utilitaire util = new Utilitaire();
+        List<Class<?>> controller = util.recupererClasseController(packageName,
+                mg.itu.miantra.annotation.Controller.class);
+        for (Class<?> c : controller) {
+            listController.add(c.getName());
+        }
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         StringBuffer url = request.getRequestURL();
@@ -27,6 +44,10 @@ public class FrontControllerServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         out.println(url);
+        out.println("Classe controller");
+        for (String controller : listController) {
+            out.println(controller);
+        }
     }
 
 }
---
+++ util/Utilitaire.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utilitaire {
+
+    public List<Class<?>> recupererClasseController(String packageName, Class<? extends Annotation> annotation) {
+        List<Class<?>> resultats = new ArrayList<>();
+        
+        return resultats;
+    }
+
+}
